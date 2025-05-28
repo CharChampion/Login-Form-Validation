1013085296726238be3e1b191bfb4da1ee429636
CharChampion <charvikpenjarla@gmail.com>
2025-05-28 17:24:15 -0400

    Login Form Validation (3)

---
+++ script.js
@@ -0,0 +1,11 @@
+if ("first_name"=""){
+    alert("You need to enter your first name.");
+}
+
+if ("last_name"=""){
+    alert("You need to enter your last name.");
+}
+
+if ("age"=""){
+    alert("You need to enter your age if you are 18 or above.");
+}
